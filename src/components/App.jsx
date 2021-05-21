5fc9db2836972e64fe8a9c22cf3724f9f1f17a54
cohlerCoded <jeffcohler@cohlercoded.com>
2021-05-21 15:45:47 -0700

    select a language

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = { language: "english" };
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+  componentDidMount() {
+    console.log(this.state);
+  }
+  componentDidUpdate() {
+    console.log(this.state);
+  }
   render() {
     return (
       <div className="ui container">
-        <h1>App</h1>
+        <div className="item">
+          Select a language:
+          <i
+            onClick={() => this.onLanguageChange("english")}
+            className="flag us"
+          />
+          <i
+            onClick={() => this.onLanguageChange("dutch")}
+            className="flag nl"
+          />
+        </div>
+        {this.state.language}
       </div>
     );
   }
