9085799662cde9991efacaf0919f4716ae25286d
cohlerCoded <jeffcohler@cohlercoded.com>
2021-05-21 16:44:08 -0700

    context value

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Field extends Component {
+  static contextType = LanguageContext;
   render() {
+    const text = this.context === "english" ? "Name" : "Naam";
     return (
       <div className="ui field">
-        <label htmlFor="name">Name </label>
+        <label htmlFor="name">{text} </label>
         <input type="text" id="name" />
       </div>
     );
